c20015e378e1594033bd9b710840e689afa6d61c
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-02-25 16:08:28 +0200

    User sign-up handling was added.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -4,17 +4,19 @@ from . import views
 
 
 urlpatterns = [
-	#Обработчик действий со статьями
+	# Обработчик действий со статьями
 	path('login/', auth_views.LoginView.as_view(), name='login'),
 	path('log-out/', auth_views.LogoutView.as_view(template_name='registration/log-out.html'), name='logout'),
 	path('', views.dashboard, name='dashboard'),
 	path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
 	path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    
+
     # Обработчики восстановления пароля.
     # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('django.contrib.auth.urls')), # Делает то же самое что и закомментированные сверху строки
+
+    path('register/', views.register, name='register'),
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .forms import UserRegistrationForm
+
 
 @login_required
 def dashboard(request):
 	return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создаем нового пользователя, но пока не сохраняем в базу данных.
+            new_user = user_form.save(commit=False)
+            # Задаем пользователю зашифрованный пароль.
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Сохраняем пользователя в базе данных.
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,'account/register.html', {'user_form': user_form})
