6d977cd1b44072df80f081a505ce1935f51e7f95
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-02-25 16:13:37 +0200

    Logout view fix.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
 	# Обработчик действий со статьями
 	path('login/', auth_views.LoginView.as_view(), name='login'),
-	path('log-out/', auth_views.LogoutView.as_view(template_name='registration/log-out.html'), name='logout'),
+	path('logout/', auth_views.LogoutView.as_view(template_name='registration/log-out.html'), name='logout'),
 	path('', views.dashboard, name='dashboard'),
 	path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
 	path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
