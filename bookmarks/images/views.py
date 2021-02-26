4b2e5c5fdd929d21758f05eaa368efc10b808d93
vital11 <1861755@gmail.com>
2021-02-26 19:23:22 +0200

    Added pagination using AJAX

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,10 +1,13 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
 from .models import Image
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
+
+from common.decorators import ajax_required
 
 
 @login_required
@@ -32,18 +35,15 @@ def image_create(request):
         # Заполняем форму данными из GET-запроса.
         form = ImageCreateForm(data=request.GET)
         
-    return render(request,
-        'images/image/create.html',
-        {'section': 'images', 'form': form})
+    return render(request, 'images/image/create.html', {'section': 'images', 'form': form})
 
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request,
-        'images/image/detail.html',
-        {'section': 'images','image': image})
+    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
 
 
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
@@ -56,7 +56,31 @@ def image_like(request):
                 image.users_like.add(request.user)
             else:
                 image.users_like.remove(request.user)
-            return JsonResponse({'status':'ok'})
+            return JsonResponse({'status': 'ok'})
         except:
             pass
-    return JsonResponse({'status':'ok'})
+    return JsonResponse({'status': 'ko'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver last page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
+    return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
+
