2a68ffcfcf4b36cfc74b60f64f4cf5d7cdf3e79c
Aleksandr Honcharuk <alexandr.gnrk@gmail.com>
2021-03-01 15:42:46 +0200

    Signal for likes was added.

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -13,7 +13,8 @@ class Image(models.Model):
 	description = models.TextField(blank=True)
 	created = models.DateField(auto_now_add=True, db_index=True)
 	users_like = models.ManyToManyField(User, related_name='images_likes', blank=True)
-
+	total_likes = models.PositiveIntegerField(db_index=True, default=0)
+	
 	def __str__(self):
 		return self.title
 
